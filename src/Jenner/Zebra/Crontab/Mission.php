37c4058a0308213dd3c23ae957a0d70cc299e4c5
huyanping <hypxm@qq.com>
2014-11-09 20:32:46 +0800

    init

---
+++ src/Jenner/Zebra/Crontab/Mission.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 14-11-7
+ * Time: 下午5:23
+ */
+
+namespace Jenner\Zebra\Crontab;
+
+
+class Mission {
+
+    protected $cmd;
+
+    protected $output_file;
+
+    public function __construct($cmd, $output_file=null){
+        $this->cmd = $cmd;
+        $this->output_file = $output_file;
+    }
+
+    public function start(){
+        $output_file = is_null($this->output_file) ? '/dev/null' : $this->output_file;
+        $cmd = $this->cmd . ' >>' .  $output_file;
+        exec($cmd);
+        exit;
+    }
+} 
