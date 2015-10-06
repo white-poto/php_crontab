1bc7f648ed442e3f99f6ab481f1ce3ebcadb41b2
huyanping <hypxm@qq.com>
2015-10-06 11:37:32 +0800

    add name space

--- src/Jenner/Crontab/HTTP/Server.php
+++ src/Jenner/Crontab/HTTP/Server.php
@@ -1,11 +1,13 @@
 <?php
-
 /**
  * Created by PhpStorm.
  * User: Jenner
  * Date: 2015/10/6
  * Time: 11:36
  */
+
+namespace Jenner\Crontab\HTTP;
+
 class Server
 {
     public function __construct($host, $port){
