d297fde702b6dd4a2fe14e9ed94dca256fa78a81
huyanping <hypxm@qq.com>
2015-10-07 09:19:07 +0800

    tests

---
+++ tests/http_daemon.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jenner
+ * Date: 2015/10/7
+ * Time: 9:18
+ */
+
+date_default_timezone_set('PRC');
+define('DS', DIRECTORY_SEPARATOR);
+require dirname(dirname(__FILE__)) . DS . 'vendor' . DS . 'autoload.php';
+
+error_reporting(E_ALL);
+
+$hello_command = "echo 'hello ';";
+$world_command = "sleep(1); echo 'world';";
+
+$missions = [
+    [
+        'name' => 'hello',
+        'cmd' => "php -r '{$hello_command}'",
+        'out' => '/tmp/php_crontab.log',
+        'time' => '* * * * *',
+        'user' => 'www',
+        'group' => 'www'
+    ],
+    'mission_ls' => [
+        'name' => 'world',
+        'cmd' => "php -r '{$world_command}'",
+        'out' => '/tmp/php_crontab.log',
+        'time' => [
+            '* * * * *',
+            '1 * * * *',
+        ],
+    ],
+];
+
+$http_daemon = new \Jenner\Crontab\HttpDaemon($missions);
+$http_daemon->start();
