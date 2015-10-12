9eded6e3bbea6eb05e3cf54a2d5f47f62846172b
huyanping <hypxm@qq.com>
2015-10-12 16:01:22 +0800

    reformat code

--- bin/crontab.php
+++ bin/crontab.php
@@ -9,3 +9,17 @@
 
 require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . 'autoload.php';
 
+class Crontab {
+    protected $args = array(
+        'h' => 'help',
+        'c:' => 'config:',
+        'p:' => 'port:',
+        'pf:' => 'pid-file:',
+        'l:' => 'log:',
+    );
+
+    public function start(){
+        $params = getopt(implode('', array_keys($this->args)), $this->args);
+
+    }
+}
