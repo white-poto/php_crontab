3a19711ccb3f92f99834fc197ae51cbb78fc8b7c
huyanping <hypxm@qq.com>
2015-10-12 15:04:17 +0800

    add MissionTest

--- tests/MissionTest.php
+++ tests/MissionTest.php
@@ -4,23 +4,14 @@
  * Created by PhpStorm.
  * User: Jenner
  * Date: 2015/10/12
- * Time: 9:28
+ * Time: 14:52
  */
-
 class MissionTest extends TestSuite
 {
     /**
      * @var \Jenner\Crontab\Mission
      */
     protected $mission;
-
-    /**
-     *
-     */
-    public function setUp()
-    {
-        $this->mission = new \Jenner\Crontab\Mission("mission_test", "ls / -al", "* * * * *", "/tmp/mission_test.log");
-    }
 
     public function testNeed()
     {
@@ -29,16 +20,6 @@ class MissionTest extends TestSuite
         $this->assertTrue($this->mission->needRun(time() + 120));
     }
 
-    public function testRun()
-    {
-        echo "prepare" . PHP_EOL;
-        $this->mission->run();
-        echo "run" . PHP_EOL;
-        $this->mission->wait();
-        echo "wait" . PHP_EOL;
-        $this->assertEquals($this->mission->exitCode(), 0);
-        $out = file_get_contents("/tmp/mission_test.log");
-        exec("ls / -al", $except);
-        $this->assertEquals($out, $except);
-    }
+
+
 }
