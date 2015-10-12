2cfba3beb1ff7b8a89116e8d258fc15095d6f808
huyanping <hypxm@qq.com>
2015-10-12 14:47:03 +0800

    add MissionTest

--- bin/runtest.php
+++ bin/runtest.php
@@ -10,5 +10,4 @@
 require_once(dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'tests' . DIRECTORY_SEPARATOR . "TestSuite.php");
 require_once(dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'tests' . DIRECTORY_SEPARATOR . "MissionTest.php");
 
-TestSuite::run("InterfaceTest");
-TestSuite::run("ProcessTest");
+TestSuite::run("MissionTest");
