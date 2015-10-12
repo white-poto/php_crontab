f75d55b0270b11d19df9d1b6b7c12c3ad65ce6a0
huyanping <hypxm@qq.com>
2015-10-12 14:44:32 +0800

    add MissionTest

---
+++ bin/runtest.php
@@ -0,0 +1,14 @@
+#!php
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jenner
+ * Date: 2015/9/14
+ * Time: 16:43
+ */
+
+require_once(dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'tests' . DIRECTORY_SEPARATOR . "TestSuite.php");
+require_once(dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'tests' . DIRECTORY_SEPARATOR . "MissionTest.php");
+
+TestSuite::run("InterfaceTest");
+TestSuite::run("ProcessTest");
