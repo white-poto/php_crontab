c0224518bd82e2a0288a6ae0008c991e6dd400fe
huyanping <hypxm@qq.com>
2015-10-12 09:28:03 +0800

    move tests files to example dir

---
+++ example/simple.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jenner
+ * Date: 14-11-9
+ * Time: 下午9:32
+ */
+
+
+date_default_timezone_set('PRC');
+define('DS', DIRECTORY_SEPARATOR);
+require dirname(dirname(__FILE__)) . DS . 'vendor' . DS . 'autoload.php';
+
+error_reporting(E_ALL);
+
+$hello_command = "sleep(1); echo 123;";
+$world_command = "sleep(1); echo 456 . PHP_EOL;";
+
+$missions = [
+    [
+        'name' => 'hello',
+        'cmd' => "php -r '{$hello_command}'",
+        'out' => '/tmp/php_crontab.log',
+        'time' => '* * * * *',
+        'user' => 'www',
+        'group' => 'www'
+    ],
+    'mission_ls' => [
+        'name' => 'world',
+        'cmd' => "php -r '{$world_command}'",
+        'out' => '/tmp/php_crontab.log',
+        'time' => '* * * * *',
+    ],
+];
+$logger = new \Monolog\Logger("php_crontab");
+$logger->pushHandler(new \Monolog\Handler\StreamHandler("/tmp/php_crontab.log"));
+
+$tasks = array();
+foreach($missions as $mission){
+    $tasks[] = new \Jenner\Crontab\Mission(
+            $mission['name'],
+            $mission['cmd'],
+            $mission['time'],
+            $mission['out']
+        );
+}
+
+$crontab_server = new \Jenner\Crontab\Crontab($logger, $tasks);
+$crontab_server->start(time());
